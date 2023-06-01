bf7a9a18bc84e9ad5defc0dd9a072c19e3278d87
Thayne Campbell <thaynecampbell@gmail.com>
2023-06-01 12:02:06 -0700

    trying to setup website

--- markdown.js
+++ markdown.js
@@ -1,15 +1,10 @@
-const fs = require('fs');
-const showdown = require('showdown');
-
-// Read the content of index.md
-const markdown = fs.readFileSync('index.md', 'utf-8');
-
-// Configure the converter
-const converter = new showdown.Converter();
-converter.setOption('tables', true); // Enable table support if needed
-
-// Convert Markdown to HTML
-const html = converter.makeHtml(markdown);
-
-// Write the HTML content to index.html
-fs.writeFileSync('index.html', html);
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Thayne-Campbell.github.io</title>
+    </head>
+<body>
+<h1>Hello World</h1>
+<p>I'm hosted with GitHub Pages.</p>
+</body>
+</html>
